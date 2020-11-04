4c9514b95256a8c92bf370c4a9487ad077889e88
Lolson31 <69780105+Lolson31@users.noreply.github.com>
2020-11-03 18:06:14 -0600

    Create jacobsthal.c

---
+++ jacobsthal.c
@@ -0,0 +1,35 @@
+/**
+ * Author: Luke Olson
+ *
+ * A recursive function that calculats the nth term of a Jacobsthal seqeunce
+ */
+
+ int main(int argc, char const *argv[])
+ {
+   if (argc != 2)
+   {
+     printf("Invalid number of arguments\n");
+     exit(1);
+   }
+
+   int term = atoi(argv[1]);
+   int result = jacobsthal(term);
+
+   printf("The %d term is %d\n",term, result);
+
+   return 0;
+ }
+
+ int jacobsthal(int term)
+ {
+   int currTerm = 0;
+   int result = 0;
+   if (currTerm = term)
+   {
+     return (result);
+   }
+   else
+   {
+     result = jacobsthal(currTerm) + jacobsthal((currTerm + 1) * 2);
+   }
+ }
